ca376995e72bb78550201e12d42ebcfef209a975
Anthony Leung <anthonyleung07@gmail.com>
2026-03-08 21:01:04 +0000

    new types from db

---
+++ lib/types.ts
@@ -0,0 +1,18 @@
+export type Profile = {
+  id: string;
+  first_name: string | null;
+  last_name: string | null;
+  created_at: string;
+  updated_at: string;
+};
+
+export type Job = {
+  id: string;
+  user_id: string;
+  company_name: string;
+  job_title: string;
+  job_url: string;
+  job_description: string | null;
+  created_at: string;
+  updated_at: string;
+};
